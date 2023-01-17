5b41f67070cce16b96575c710a0104aa9f27719e
Carlos <carlitosviano@gmail.com>
2023-01-17 12:56:09 +0100

    login working with api and bbdd

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,64 +2,79 @@ import Button from '../Button/button'
 import './Login.css'
 import {useState} from 'react';
 
+
 export default function Login(){
 
-    const usuario = {email: "carlitosviano@gmail.com", password: "123456"}
-
-    const [user, setUser] = useState({
+    const  [newUser, setNewUser] = useState({
         email:"",
-        password:""
-    })
-    console.log(user)
-    function handleUser(event){
-        const newUser = {
-            ...user,
-            [event.target.name]:event.target.value
-        };
-        setUser(newUser)
-    }
+        password:"",
+        name:"",
+        surname:""
+       })
 
-    function checkUser(event){
-        event.preventDefault()
-        if(user.email === usuario.email && user.password === usuario.password){
-            alert("Hola")
-        } else { alert("Mal")}
+    function handleInput(e){
+        const newRegister = {
+            ...newUser,
+            [e.target.name]:e.target.value
+        }
+        setNewUser(newRegister)
+       }
+
+    function registerAcc(e){
+        e.preventDefault()
+        fetch("http://localhost:3000/user",{
+            method:"POST",
+            headers: {
+                "Content-type": "application/json"
+            }
+        ,
+        body: JSON.stringify(newUser)
+         }).then((response) => {
+            console.log(response.status);
+            if(response.status === 400){
+                alert('Error al recibir el body')
+            } else if(response.status === 200){
+                alert(`Usuario ${newUser.name} registrado correctamente`)
+            } else if(response.status === 409){
+                alert('Usuario ya registrado')
+            }
+         })
     }
 
     return (    
         <div className='mainContainer'> 
         <div className='login'>
-        <form onSubmit={checkUser} className='formLogin'>
+        <form className='formLogin'>
             <h5>Login</h5>
             <div className='inputContainer'>
                 <h6>Email</h6>
-                <input type='text' name='email' value={user.email} onChange={handleUser} className='loginInput' placeholder=' '/>
+                <input type='text' name='email' className='loginInput'/>
             </div>
             <div className='inputContainer'>
                 <h6>Password</h6>
-                <input type='password' name='password' value={user.password} onChange={handleUser} className='loginInput' placeholder=' '/>
+                <input type='password' name='password'className='loginInput'/>
             </div>
             <Button value='Sign In'/>
         </form>
     </div>
             <div className='register'>
-            <form className='formRegister'>
+            <form className='formRegister' onSubmit={registerAcc}>
                 <h5>Create Account</h5>
                 <div className='inputContainer'>
                     <h6>First Name</h6>
-                    <input type='text' className='registerInput' placeholder=' '/>
+                    <input type='text' className='registerInput' name='name' value={newUser.name} onChange={handleInput}/>
                 </div>
                 <div className='inputContainer'>
                     <h6>Last Name</h6>
-                    <input type='text' className='registerInput' placeholder=' '/>
+                    <input type='text' className='registerInput' name='surname' value={newUser.surname} onChange={handleInput}/>
                 </div>
                 <div className='inputContainer'>
                     <h6>Email</h6>
-                    <input type='text' className='registerInput' placeholder=' '/>
+                    <input type='text' className='registerInput' name='email' value={newUser.email} onChange={handleInput}/>
                 </div>
                 <div className='inputContainer'>
                     <h6>Password</h6>
-                    <input type='password' className='registerInput' placeholder=' '/>
+                    <input type='password' className='registerInput' name='password' value={newUser.password} onChange={handleInput}/>
                 </div>
                 <Button value='Create'/>
             </form>
